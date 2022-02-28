fec31498f157a1069503fa908b7cb77a0f47fe66
Tyler Moson <tyler.p.moson@gmail.com>
2022-02-27 21:21:06 -0500

    added some tracing, fixed duplicated moves in list of moves for main class

--- node.py
+++ node.py
@@ -4,7 +4,8 @@ from state import State
 from move import Move
 
 class Node:
-  def __init__(self, state: State, move: Move, parent):
+  def __init__(self, state: State, move: Move, number: int, parent):
+    self.name = "N%s" % number
     self.state = state
     self.move = move
     self.parent = parent
@@ -13,7 +14,11 @@ class Node:
     return self.state.equals(node2.state)
     
   def display(self):
-    print(self.move.desc)
+    print(self.name)
+    if(not self.parent == None):
+      print("Parent: ", self.parent.name)
+    if(not self.move == None):
+      print(self.move.desc)
     print("State: ")
     self.state.display()
 
--- solver.py
+++ solver.py
@@ -6,33 +6,39 @@ from node import Node
 
 class Solver:
   def __init__(self, state: State, moves: TypedDict):
-    self.root = Node(state, None, None)
+    self.root = Node(state, None, 0, None)
     self.moves = moves
 
-  def explored_state(self, comp_state: State, explored: TypedDict):
-    for state in explored:
-      if(comp_state.equals(state)):
+  def explored_state(self, comp_state: Node, explored: TypedDict):
+    for node in explored:
+      if(comp_state.equals(node)):
+        print("skipping state")
         return True
     return False
 
   def solve(self):
-    num_explored = 0
+    num_nodes = 0
     explored = []
     unexplored = [self.root]
     while(unexplored != []):
       node = unexplored.pop()
       explored.append(node)
-      print("Exploring State: ")
-      node.state.display()
-      if(not node.state.feast()):
-        for move in self.moves:
-          if(move.applicable(node.state)):
-            child = Node(move.move(node.state), move, node)
-            if(child.state.goal()):
-              print("Found a goal state!")
-              child.display_solution()
-              return
-            if(not self.explored_state(child, explored)):
-              print("appended unexplored state!")
-              unexplored.append(child)
+      # uncomment this region for easier tracing
+      # print("E-node >>>>>>>>>>>>>>>>>>>>")
+      # node.display()
+      # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>")
+      if(node.state.goal()):
+        node.display_solution()
+        return
+      for move in self.moves:
+        if(move.applicable(node.state)):
+          num_nodes += 1
+          child = Node(move.move(node.state), move, num_nodes, node)
+          if(not self.explored_state(child, explored) and not child.state.feast()):
+            unexplored.append(child)
+      # uncomment this region for easier tracing
+      # print("UNEXPLORED: >>>>>>>>>>>>>>>>>>>>")
+      # for node in unexplored:
+      #   node.display()
+      # print(">>>>>>>>>>>>>>>>>>>>>>>>>>>")
     print("There is no solution")
